a77f1531ffeee526f6445039e5fd7285dd6fe59c
Willy Ovalle <willyovalle16@gmail.com>
2018-01-31 19:40:28 -0400

    Adding README

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,5 @@
 const functions = require('firebase-functions');
 
 exports.helloWorld = functions.https.onRequest((req, res) => {
-    console.log(req.method)
     res.send("Hello from Firebase!");
 });
