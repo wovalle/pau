a418cf0d9d70e2bdbf5ce46422017c9855d79b9e
Willy Ovalle <willyovalle16@gmail.com>
2018-02-11 18:36:17 -0400

    Enhancement: refactoring

---
+++ functions/helpers/tgMessage.helper.js
@@ -0,0 +1,40 @@
+const get = require('lodash.get');
+
+exports.flattenTgMessage = (message) => {
+  const entity = get(message, 'entities.0', { offset: 0, length: 0 });
+  const text = get(message, 'text', '').slice(1);
+  return Object.assign({
+    text,
+    isBotCommand: get(entity, 'type') === 'bot_command',
+    command: text.slice(entity.offset, entity.length).trim(),
+    chatId: get(message, 'chat.id'),
+    fromId: get(message, 'from.id'),
+    fromUsername: get(message, 'from.username'),
+    messageId: get(message, 'message_id'),
+    date: new Date(message.date),
+  });
+};
+
+exports.codewarsExercises = (exercises) => {
+  const status = {
+    missing: '🔥',
+    done: '💯',
+    late: '🕑',
+  };
+
+  const batchs = exercises.reduce((acc, cur) => {
+    acc[cur.batch] = (acc[cur.batch] || []).concat(cur);
+    return acc;
+  }, {});
+
+  return Object.keys(batchs).reduce((acc, cur) => {
+    const rows = batchs[cur].map(ex => `[${ex.name}](${ex.url}) (${status[ex.status]})`);
+
+    // eslint-disable-next-line no-param-reassign
+    acc += `*Batch #${cur}* \n${rows.join('\n')}\n\n`;
+
+    return acc;
+  }, '');
+};
+
+exports.genericError = () => 'An error has occurred, please contact the administrator';
--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const TelegramBot = require('node-telegram-bot-api');
-const get = require('lodash.get');
 const { v4 } = require('uuid');
 const http = require('got');
 
 const { applicationName, commands } = require('./constants');
-const { formatExcercises } = require('./helpers/messageFormatter');
+const { codewarsExercises, genericError, flattenTgMessage } = require('./helpers/tgMessage.helper');
 
 const EmailService = require('./services/email.service');
 const CodewarsService = require('./services/codewars.service');
@@ -19,45 +18,30 @@ const bot = new TelegramBot(functions.config().telegram.token);
 const logger = console;
 admin.initializeApp(functions.config().firebase);
 
-const parseTelegramMessage = (message) => {
-  const entity = get(message, 'entities.0', { offset: 0, length: 0 });
-  const text = get(message, 'text', '').slice(1);
-  return Object.assign(
-    message,
-    {
-      parsed: {
-        text,
-        isBotCommand: get(entity, 'type') === 'bot_command',
-        command: text.slice(entity.offset, entity.length).trim(),
-        chatId: get(message, 'chat.id'),
-      },
-    });
-};
-
 const loginHandler = (message) => {
-  const matchSchoolId = /\d{8}/.exec(message.parsed.text);
+  const matchSchoolId = /\d{8}/.exec(message.text);
 
   if (!matchSchoolId) {
-    bot.sendMessage(message.parsed.chatId, 'Invalid school id provided.');
+    bot.sendMessage(message.chatId, 'Invalid school id provided.');
     return;
   }
 
-  const studentId = message.parsed.text.slice(matchSchoolId.index, matchSchoolId.index + 8).trim();
+  const studentId = message.text.slice(matchSchoolId.index, matchSchoolId.index + 8).trim();
   const studentRef = admin.database().ref(`/students/${studentId}`);
 
   studentRef.once('value', (result) => {
     const student = result.val();
 
     if (!student) {
-      return bot.sendMessage(message.chat.id, `Invalid student id ${studentId}`);
+      return bot.sendMessage(message.chatId, `Invalid student id ${studentId}`);
     }
 
     const confirmationCode = v4();
-    const confirmationUrl = `${functions.config().functions['email-confirm-url']}?code=${confirmationCode}&tid=${message.from.id}`;
+    const confirmationUrl = `${functions.config().functions['email-confirm-url']}?code=${confirmationCode}&tid=${message.fromId}`;
 
     studentRef.update({
       confirmation_code: confirmationCode,
-      confirmation_user: message.from.id,
+      confirmation_user: message.fromId,
     });
 
     emailService.sendConfirmationCodeEmail(student.email, confirmationUrl, () => {
@@ -66,7 +50,7 @@ const loginHandler = (message) => {
       const lastLetters = eParts[0].slice(-2);
 
       const filteredEmail = `${initialLetters}${'*'.repeat(eParts[0].length - 2)}${lastLetters}@${eParts[1]}`;
-      bot.sendMessage(message.chat.id, `We have sent you a confirmation link to ${filteredEmail}`);
+      bot.sendMessage(message.chatId, `We have sent you a confirmation link to ${filteredEmail}`);
     });
   });
 };
@@ -77,7 +61,7 @@ const codewarsHandler = (message) => {
     .database()
     .ref('/students')
     .orderByChild('telegram_id')
-    .equalTo(message.from.id)
+    .equalTo(message.fromId)
     .limitToFirst(1);
 
   studentQuery.once('value', (queryResult) => {
@@ -100,24 +84,28 @@ const codewarsHandler = (message) => {
         exercisesRef.once('value', (result) => {
           const fbExercises = result.val();
           const diff = diffExcercises(fbExercises, codewarsExs);
-          bot.sendMessage(message.parsed.chatId, formatExcercises(diff), { parse_mode: 'Markdown', disable_web_page_preview: true });
+          bot.sendMessage(message.chatId, codewarsExercises(diff), { parse_mode: 'Markdown', disable_web_page_preview: true });
         });
+      })
+      .catch((error) => {
+        logger.error(error);
+        bot.sendMessage(message.chatId, genericError());
       });
   });
 };
 
 exports.telegramHook = functions.https.onRequest((req, res) => {
-  const message = parseTelegramMessage(req.body.message);
+  const message = flattenTgMessage(req.body.message);
 
-  if (req.method !== 'POST' || !message.parsed.chatId) {
+  if (req.method !== 'POST' || !message.chatId) {
     return res.status(403).send('Forbidden');
   }
 
-  if (!message.parsed.isBotCommand) {
-    bot.sendMessage(message.parsed.chatId, `Can't understand text "${message.text}", use /help to see available commands`);
+  if (!message.isBotCommand) {
+    bot.sendMessage(message.chatId, `Can't understand text "${message.text}", use /help to see available commands`);
     return res.sendStatus(200);
   }
-  switch (message.parsed.command) {
+  switch (message.command) {
     case commands.login:
       loginHandler(message);
       break;
@@ -125,7 +113,7 @@ exports.telegramHook = functions.https.onRequest((req, res) => {
       codewarsHandler(message);
       break;
     default:
-      bot.sendMessage(message.parsed.chatId, `Command "${message.text}" is not valid.`);
+      bot.sendMessage(message.chatId, `Command "${message.text}" is not valid.`);
   }
 
   return res.sendStatus(200);
